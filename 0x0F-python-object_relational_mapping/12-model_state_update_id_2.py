cf33e37759824a5faf10be8c34d23677bfd94957
mercyswan <mercymusa4@gmail.com>
2022-09-09 00:06:05 -0700

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 """
-changes the name of the State object where id=2 to New Mexico from a database
+changes the name of a State object from the database hbtn_0e_6_usa
 """
 
-import sqlalchemy
+import sys
+from unicodedata import name
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
-from model_state import Base, State
-
+from model_state import State
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
-    session = Session()
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
     state = session.query(State).filter_by(id=2).first()
     state.name = "New Mexico"
     session.commit()
-    session.close()
