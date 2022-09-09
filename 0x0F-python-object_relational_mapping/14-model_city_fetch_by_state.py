bb017b60d5b20b1369c3e6103607aab91259df01
mercyswan <mercymusa4@gmail.com>
2022-09-09 00:14:41 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """
-prints all City objects from a database
+prints all City objects from the database hbtn_0e_14_usa
 """
 
-import sqlalchemy
-from sqlalchemy import create_engine
+import sys
+from venv import create
+from sqlalchemy import create_engine, true
 from sqlalchemy.orm import sessionmaker
-from sys import argv
-from model_state import Base, State
+from model_state import State
 from model_city import City
 
-
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
-    session = Session()
-    rows = session.query(City, State).filter(City.state_id == State.id)\
-                                     .order_by(City.id).all()
-    for city, state in rows:
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    for city, state in session.query(City, State)\
+        .filter(City.state_id == State.id)\
+            .order_by(City.id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.close()
