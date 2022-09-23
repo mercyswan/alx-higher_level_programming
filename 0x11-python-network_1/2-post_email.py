a9dfa3715e987874b230ac39181d02b71e3b2873
mercyswan <mercymusa4@gmail.com>
2022-09-23 14:35:03 -0700

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
+"""A script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
+"""
+import sys
+import urllib.parse
+import urllib.request
 
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))#!/usr/bin/python3
+"""A script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
 """
 import sys
 import urllib.parse
